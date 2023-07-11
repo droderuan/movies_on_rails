e8536718448cf27ede7e982ac56acb0eedebdd80
Ruan Ferreira <droderuan@hotmail.com>
2023-07-11 00:19:21 -0300

    feat: add api version to api url and created a new index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root "movies#index"
+  scope(:path => '/api/v1') do
 
-  resources :movies, only: %i[index] do
-    get :recommendations, on: :collection
-    get :user_rented_movies, on: :collection
-    get :rent, on: :member
+    root "application#index"
+
+
+    resources :movies, only: %i[index] do
+      get :recommendations, on: :collection
+      get :user_rented_movies, on: :collection
+      get :rent, on: :member
+    end
   end
 end

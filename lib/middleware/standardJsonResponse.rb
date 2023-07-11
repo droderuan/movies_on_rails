3976edcd4b8a1ce31b73533ab9e972fdc8a445aa
Ruan Ferreira <droderuan@hotmail.com>
2023-07-11 02:34:37 -0300

    fix: fix middleware standardize and app index

--- lib/middleware/standardJsonResponse.rb
+++ lib/middleware/standardJsonResponse.rb
@@ -9,10 +9,18 @@ module Middleware
 
     def call(env)
       status, headers, response = @app.call(env)
-      body = JSON.parse(response.body)
-      new_body = {"data": body}.to_json
-      headers['Content-Length'] = new_body.bytesize.to_s
-      [status, headers, [new_body]]
+      begin
+        if response.respond_to? :body
+          body = JSON.parse(response.body)
+          new_body = {"data": body}.to_json
+          headers['Content-Length'] = new_body.bytesize.to_s
+          [status, headers, [new_body]]
+        else
+          [status, headers, response]
+        end
+      rescue
+        [status, headers, response]
+      end
     end
   end
 end
